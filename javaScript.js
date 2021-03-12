023a363c8cf739b6b9cb4faf7fd83222dbb151ef
fullCase <72561970+fullCase@users.noreply.github.com>
2021-03-12 22:51:54 +0100

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -2,6 +2,13 @@ const filter = document.getElementById("filter");
 const button = document.getElementById("filterButton");
 const button2 = document.getElementById("filterContainerButton");
 
+window.addEventListener("scroll", scrollMenu);
+
+function scrollMenu(){
+    console.log("ggg");
+    document.querySelector(".navbar").classList.toggle("fixed-top", window.scrollY > 290);
+}
+
 button.addEventListener("click", function(){
     console.log(window.innerWidth);
     if(window.innerWidth < 576){
