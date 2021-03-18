bd7441a4fecbc3e35c1d0c8bcb0fda2629542106
Łukasz Odolczyk <72561970+fullCase@users.noreply.github.com>
2021-03-18 20:56:34 +0100

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,28 +1,10 @@
 const filter = document.getElementById("filter");
 const button = document.getElementById("filterButton");
-const button2 = document.getElementById("filterContainerButton");
+const buttonInFilter = document.getElementById("buttonInFilter");
 
 window.addEventListener("scroll", scrollMenu);
 
 function scrollMenu(){
-    console.log("ggg");
     document.querySelector(".navbar").classList.toggle("fixed-top", window.scrollY > 290);
 }
 
-button.addEventListener("click", function(){
-    console.log(window.innerWidth);
-    if(window.innerWidth < 576){
-        filter.classList.remove("d-none");
-        filter.classList.toggle("on");
-        console.log("ssss");
-    }    
-});
-
-button2.addEventListener("click", function(){
-    console.log(window.innerWidth);
-    if(window.innerWidth < 576){
-        filter.classList.remove("on");
-        filter.classList.toggle("d-none");
-        console.log("nnnn");
-    }    
-});
