ccf3b8b68c6add7f430277c9763d585160bb5267
Łukasz Odolczyk <72561970+fullCase@users.noreply.github.com>
2021-03-25 13:09:56 +0100

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,12 @@
 const filter = document.getElementById("filter");
-const button = document.getElementById("filterButton");
+const filterButton = document.getElementById("filterButton");
 const buttonInFilter = document.getElementById("buttonInFilter");
 
 window.addEventListener("scroll", scrollMenu);
+
+filterButton.addEventListener("click", function(){
+    filter.classList.add("filter_popup");
+});
 
 function scrollMenu(){
     document.querySelector(".navbar").classList.toggle("fixed-top", window.scrollY > 290);
